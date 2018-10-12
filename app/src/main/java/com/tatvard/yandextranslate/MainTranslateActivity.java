2b65abb5b767b061bf94531dda9a2fad9644d64d
Tatev <tatevikvardanyan21@gmail.com>
2018-10-12 23:00:06 +0400

    add main translate screen

---
+++ app/src/main/java/com/tatvard/yandextranslate/MainTranslateActivity.java
@@ -0,0 +1,19 @@
+package com.tatvard.yandextranslate;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainTranslateActivity extends AppCompatActivity {
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_translate);
+    }
+}
